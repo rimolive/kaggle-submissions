eed4e95abf810fb2b63a2e01d4455c56f2b06cca
Ricardo Martinelli de Oliveira <ricardo.martinelli.oliveira@gmail.com>
2018-05-27 10:01:41 -0300

    Visualizations for house prices

--- house-prices/house-prices.R
+++ house-prices/house-prices.R
@@ -1,5 +1,19 @@
 library(tidyverse)
 
+setwd("house-prices")
 
 houses.train <- read.csv("train.csv")
 houses.test <- read.csv("test.csv")
+
+par(mfrow=c(1,2))
+ggplot(houses.train, aes(MSSubClass, SalePrice)) +
+  geom_point()
+ggplot(houses.train, aes(MSZoning, SalePrice)) +
+  geom_point()
+ggplot(houses.train, aes(YearBuilt, SalePrice, color=MSZoning)) +
+  geom_point()
+
+ggplot(houses.train, aes(MSSubClass)) +
+  geom_histogram(binwidth = 10)
+ggplot(houses.train, aes(YearBuilt)) +
+  geom_histogram(binwidth = 5)
